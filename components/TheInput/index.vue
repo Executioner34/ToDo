4f3b13915c2d7b598129f3e8887b35753164feae
Alexander Gorlenko <goral92@inbox.ru>
2023-01-19 14:07:43 +0300

    feat: добавит компонент ввода текста RTI-2194 RTI-2203

--- components/TheInput/index.vue
+++ components/TheInput/index.vue
@@ -1,35 +1,46 @@
 <template>
-  <div class="the-input">
-    mock$END$
-  </div>
+  <input
+    v-model="data"
+    type="text"
+    :placeholder="placeholder"
+    class="the-input-component"
+    @keyup.enter="onEnter"
+  >
 </template>
 
 <script>
 /**
- * @module components/TheInput/TheInput.vue
- * @desc component_description
- * @vue-prop {type} name - desc
- * @vue-data {type} name - desc
- * @vue-computed {type} name - desc
+ * @module components/TheInput/index.vue
+ * @desc компонент поля ввода
+ * @vue-prop {String} placeholder - placeholder для инпута
+ * @vue-data {String} data - текст поля инпута
+ * @vue-event {String} onEnter - возвращает введенный текст по нажатию клавиши Enter
  */
 export default {
   name: 'TheInput',
+  props: {
+    placeholder: {
+      type: String,
+      required: false,
+      default: 'Введите текст'
+    }
+  },
+  data() {
+    return {
+      data: '',
+    }
+  },
+  methods: {
+    onEnter() {
+      this.$emit('onEnter', this.data)
+      this.data = ''
+    }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
-.the-input {
+.the-input-component {
 }
 
-@include tablet {
-
-  .the-input {
-  }
-}
-
-@include mobile {
-
-  .the-input {
-  }
-}
 </style>
