27980f78ce9223bbb5cba13aa60c2372d3a4f85a
Alexander Gorlenko <goral92@inbox.ru>
2023-01-19 11:02:11 +0300

    feat: добавит в проект поддержку sass и изменит заголовок в nuxt.config RTI-2194 RTI-2202

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'nuxt-todo',
+    title: 'ToDo List',
     htmlAttrs: {
       lang: 'en',
     },
@@ -14,16 +14,12 @@ export default {
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+  components: false,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
@@ -31,9 +27,7 @@ export default {
     '@nuxtjs/stylelint-module',
   ],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
   modules: [],
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
+  
   build: {},
 }
