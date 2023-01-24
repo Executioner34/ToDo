109f866641b5af2195c9d8786d25762777ebd20a
Alexander Gorlenko <goral92@inbox.ru>
2023-01-24 21:10:45 +0300

    feat: добавит функцию удаления из localStorage и рефакторинг кода RTI-2194

--- api/LocalStorage/index.js
+++ api/LocalStorage/index.js
@@ -1,10 +1,20 @@
+/**
+ * @module api/LocalStorage/index.js
+ * @desc - апи для работы с localStorage.
+ * @desc - getItem(key: string) - возвращает данные по ключу из localStorage.
+ * @desc - saveItem(props: {key: string, value: Object}) - добавляет в localStorage данные по ключу.
+ * Если нет таких данных, то создаёт новый массив и записывает в localStorage
+ * @desc - updateItem(props: {key: string, value: Object, id: number}) - обновляет элемент массива в localStorage по id
+ * @desc - deleteItem(props: {key: string, id: number}) - удаляет элемент массива из localStorage по id
+ * @desc - destroy() - полностью очищает localStorage
+ */
 class LocalStorage {
   getItem(key) {
     return JSON.parse(localStorage.getItem(key))
   }
 
   saveItem(props) {
-    const {key , value} = props;
+    const { key, value } = props
     const arr = this.getItem(key)
     if (arr === null) {
       localStorage.setItem(key, JSON.stringify([value]))
@@ -15,19 +25,19 @@ class LocalStorage {
   }
 
   updateItem(props) {
-    const {key, value, id} = props;
-    const arr = this.getItem(key);
-    const ind = arr.findIndex(item => item.id === id);
+    const { key, value, id } = props
+    const arr = this.getItem(key)
+    const ind = arr.findIndex((item) => item.id === id)
     if (ind !== -1) {
-      arr[ind] = value;
+      arr[ind] = value
       localStorage.setItem(key, JSON.stringify(arr))
     }
   }
 
   deleteItem(props) {
-    const {key, id} = props;
-    const arr = this.getItem(key);
-    const newArr = arr.filter(item => item.id !== id);
+    const { key, id } = props
+    const arr = this.getItem(key)
+    const newArr = arr.filter((item) => item.id !== id)
     localStorage.setItem(key, JSON.stringify(newArr))
   }
 
@@ -36,4 +46,4 @@ class LocalStorage {
   }
 }
 
-export default new LocalStorage
+export default new LocalStorage()
---
+++ components/common/RadioInput/RadioInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="radio-input-component">
+    mock
+  </div>
+</template>
+
+<script>
+/**
+ * @module components/RadioInput.vue
+ * @desc component_description
+ * @vue-prop {type} name - desc
+ * @vue-data {type} name - desc
+ * @vue-computed {type} name - desc
+ */
+export default {
+  name: 'RadioInput',
+};
+</script>
+
+<style lang="scss" scoped>
+.radio-input-component {
+}
+
+</style>
