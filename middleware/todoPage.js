436a125a015da2fe12ac87331ad6d7ab379e214e
Alexander Gorlenko <goral92@inbox.ru>
2023-01-24 21:15:39 +0300

    docs: добавит документацию RTI-2194

--- middleware/todoPage.js
+++ middleware/todoPage.js
@@ -1,4 +1,8 @@
-export default function ({store, redirect}) {
+/**
+ * @module middleware/todoPage.js
+ * @desc - Защищает маршрут /todo/:id от отсутсвия задач в сторе
+ */
+export default function ({ store, redirect }) {
   if (!store.state.tasks.tasks.length) {
     return redirect('/')
   }
