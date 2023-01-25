08f2a0fa514c24858829ecb4e000f6e3c6478092
Alexander Gorlenko <goral92@inbox.ru>
2023-01-25 21:08:42 +0300

    feat: добавит компонент CheckBox RTI-2194

---
+++ components/common/CheckBox/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <input
+    :checked="checked"
+    class="check-box-component"
+    type="checkbox"
+    @change="checkBoxHandler"
+  />
+</template>
+
+<script>
+/**
+ * @module components/common/CheckBox/index.vue
+ * @desc компонент чекбокса. Просто слушает событие изменения состояния чекбокса и опеващает родителя о нем
+ * @vue-prop {Boolean} checked - состояние чекбокса
+ * @vue-event {Void} click - событие изменения чекбокса
+ */
+export default {
+  name: 'CheckBox',
+  props: {
+    checked: {
+      type: Boolean,
+      required: false,
+      default: false,
+    }
+  },
+  methods: {
+    checkBoxHandler() {
+      this.$emit('click')
+    }
+  }
+};
+</script>
+
