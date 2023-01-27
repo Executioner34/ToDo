5e59cea0a813822718d4d6c3b176e3254ac23438
Alexander Gorlenko <goral92@inbox.ru>
2023-01-27 17:33:36 +0300

    feat: добавит компонент обёртки для контента RTI-2194

---
+++ components/general/AppContainer/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="app-container-component">
+    <slot/>
+  </div>
+</template>
+
+<script>
+/**
+ * @module components/general/AppContainer/index.vue
+ * @desc компонент обёртки
+ */
+export default {
+  name: 'AppContainer',
+};
+</script>
+
+<style lang="scss" scoped>
+.app-container-component {
+  max-width: 1366px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 20px;
+}
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,18 @@
 <template>
-  <div class="index-page-component">
-    <add-task class="add" />
-    <task-items-list class="items-list" />
-    <filters-list class="filters" />
-  </div>
+  <app-container>
+    <div class="index-page-component">
+      <add-task class="add" />
+      <task-items-list class="items-list" />
+      <filters-list class="filters" />
+    </div>
+  </app-container>
 </template>
 
 <script>
 import TaskItemsList from '~/components/views/index/TaskItemsList/index.vue'
 import AddTask from '~/components/views/index/AddTask/index.vue'
 import FiltersList from '~/components/views/index/FiltersList/index.vue'
+import AppContainer from "~/components/general/AppContainer/index.vue";
 
 /**
  * @module pages/index.vue
@@ -18,6 +21,7 @@ import FiltersList from '~/components/views/index/FiltersList/index.vue'
 export default {
   name: 'IndexPage',
   components: {
+    AppContainer,
     FiltersList,
     AddTask,
     TaskItemsList,
